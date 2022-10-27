204a8a13700b4ec1f7994ba4ce92d346cc0dd195
unknown <you@example.com>
2022-10-27 22:39:56 +0530

    Added : Parametrised test to validate multiple entry

--- test/java/com/bridgelabz/junit/UserRegistrationTest.java
+++ test/java/com/bridgelabz/junit/UserRegistrationTest.java
@@ -1,96 +1,27 @@
 package com.bridgelabz.junit;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class UserRegistrationTest {
 
-    @Test
-    void firstNameHappyTest() throws UserRegistrationException {
-        Assertions.assertTrue(UserRegistration.firstName("Ullas"));
-        Assertions.assertFalse(UserRegistration.firstName("ullas"));
-    }
-    @Test
-    void lastNameHappyTest() throws UserRegistrationException {
-        Assertions.assertTrue(UserRegistration.lastName("Kumar"));
-        Assertions.assertFalse(UserRegistration.lastName("kumar"));
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+            "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"})
+    public void validEmails(String emailId) throws UserRegistrationException {
+        Assertions.assertTrue(UserRegistration.emailIdValidator(emailId));
     }
 
-    @Test
-    void emailHappyTest() throws UserRegistrationException {
-        Assertions.assertTrue(UserRegistration.email("ullaskumark278887941211@gmail.com"));
-        Assertions.assertFalse(UserRegistration.email("ullas"));
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "abc..", "abc@abc@gmail.com", "abc@.com.my",
+            "abc123@gmail.a", "abc123@.com",
+            "abc123@.com.com", ".abc@abc.com",
+            "abc()*@gmail.com", "abc..2002@gmail.com", "abc@abc@gmail.com",
+            "abc@gmail.com.1a", "abc..@gmail.com",
+            "abc@gmail.com.aa.au"})
+    public void invalidEmails(String emailId) throws UserRegistrationException {
+        Assertions.assertFalse(UserRegistration.emailIdValidator(emailId));
     }
-    @Test
-    void phoneNumberHappyTest() throws UserRegistrationException {
-        Assertions.assertTrue(UserRegistration.phoneNumber("91 1111111111"));
-        Assertions.assertFalse(UserRegistration.phoneNumber("911561561156"));
-    }
-    @Test
-    void passwordRule1HappyTest() throws UserRegistrationException{
-        Assertions.assertTrue(UserRegistration.passwordRule1("f4#561AS"));
-        Assertions.assertFalse(UserRegistration.passwordRule1("esf523S"));
-    }
-
-    @Test
-    void passwordRule2HappyTest() throws UserRegistrationException{
-        Assertions.assertTrue(UserRegistration.passwordRule2("daU#$23Sd"));
-        Assertions.assertFalse(UserRegistration.passwordRule2("sadkh#@4"));
-    }
-
-    @Test
-    void passwordRule3HappyTest() throws UserRegistrationException{
-        Assertions.assertTrue(UserRegistration.passwordRule3("gS#4refF"));
-        Assertions.assertFalse(UserRegistration.passwordRule3("sssHSa%$"));
-    }
-    @Test
-    void passwordRule4HappyTest() throws UserRegistrationException{
-        Assertions.assertTrue(UserRegistration.passwordRule4("uLas23@23"));
-        Assertions.assertFalse(UserRegistration.passwordRule4("DSAsDSE42"));
-    }
-
-    @Test
-    void firstNameSadTest() throws UserRegistrationException {
-        Assertions.assertFalse(UserRegistration.firstName("Ullas"));
-        Assertions.assertTrue(UserRegistration.firstName("ullas"));
-    }
-    @Test
-    void lastNameSadTest() throws UserRegistrationException {
-        Assertions.assertFalse(UserRegistration.lastName("Kumar"));
-        Assertions.assertTrue(UserRegistration.lastName("kumar"));
-    }
-
-    @Test
-    void emailSadTest() throws UserRegistrationException {
-        Assertions.assertFalse(UserRegistration.email("ullaskumark278887941211@gmail.com"));
-        Assertions.assertTrue(UserRegistration.email("ullas"));
-    }
-    @Test
-    void phoneNumberSad() throws UserRegistrationException {
-        Assertions.assertFalse(UserRegistration.phoneNumber("91 1111111111"));
-        Assertions.assertTrue(UserRegistration.phoneNumber("911561561156"));
-    }
-    @Test
-    void passwordRule1SadTest() throws UserRegistrationException{
-        Assertions.assertFalse(UserRegistration.passwordRule1("f4#561AS"));
-        Assertions.assertTrue(UserRegistration.passwordRule1("esf523S"));;
-    }
-
-    @Test
-    void passwordRule2SadTest() throws UserRegistrationException{
-        Assertions.assertFalse(UserRegistration.passwordRule2("daU#$23Sd"));
-        Assertions.assertTrue(UserRegistration.passwordRule2("sadkh#@4"));
-    }
-
-    @Test
-    void passwordRule3SadTest() throws UserRegistrationException{
-        Assertions.assertFalse(UserRegistration.passwordRule3("gS#4refF"));
-        Assertions.assertTrue(UserRegistration.passwordRule3("sssHSa%$"));
-    }
-    @Test
-    void passwordRule4SadTest() throws UserRegistrationException{
-        Assertions.assertFalse(UserRegistration.passwordRule4("uLas23@23"));
-        Assertions.assertTrue(UserRegistration.passwordRule4("DSAsDSE42"));
-    }
-
 }
